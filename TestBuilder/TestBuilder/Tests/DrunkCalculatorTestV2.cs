86636ed61f63e329fab86404732d252f9b514f45
Luciano Alves <lucianorbc@gmail.com>
2018-12-10 19:28:00 +0000

    Added another version of tests using latest test helper

---
+++ TestBuilder/TestBuilder/Tests/DrunkCalculatorTestV2.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using TestBuilder.Helpers;
+using TestBuilder.Services.Implementations;
+
+namespace TestBuilder.Tests
+{
+    [TestFixture]
+    public class DrunkCalculatorTestV2 : AutoFixtureTestBase<DrunkCalculator>
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void Add_WhenSameNumberAdded_ReturnsZero()
+        {
+            //Arrange
+            var expectedResult = 0;
+
+            //Act
+            var result = Subject.Add(1, 1);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult), $"Expected result equal to {expectedResult}");
+        }
+
+        [Test]
+        [TestCase(1, 2)]
+        [TestCase(3, 2)]
+        public void Add_WhenDifferentNumbersAdded_ReturnsCorrectCalculation(int left, int right)
+        {
+            //Arrange
+            var expectedResult = left + right;
+
+            //Act
+            var result = Subject.Add(left, right);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult), $"Expected result equal to {expectedResult}");
+        }
+    }
+}
+
